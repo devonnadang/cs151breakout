6d5ac8faed77d3af4df0dbd38b80a69fd1a1408c
Devonna Dang <dang.devonna@gmail.com>
2021-04-11 20:20:25 -0700

    Update BreakoutTest.java

--- src/breakout/BreakoutTest.java
+++ src/breakout/BreakoutTest.java
@@ -6,9 +6,89 @@ import org.junit.jupiter.api.Test;
 
 class BreakoutTest {
 
+
+	//All the blocks are cleared from board.
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void winGameTest() {
+
 	}
+
+	//User loses all lives
+	@Test
+	void loseGameTest() {
+
+	}
+
+	@Test
+	void isAliveTest(){
+		
+	}
+
+	@Test
+	void resetGameTest() {
+
+	}
+
+	@Test
+	void checkScoreTest(){
+
+	}
+
+	@Test
+	void get10HighestScores(){
+
+	}
+
+	@Test
+	void addScoreTest(){
+
+	}
+
+	@Test
+	void createBoardTest(){
+
+	}
+	
+	@Test 
+	void createBallTest(){
+
+	}
+
+	@Test
+	void createBlockTest(){
+
+	}
+
+	@Test 
+	void createPaddleTest(){
+		
+	}
+
+	//Check if score is saved in leaderboard
+	@Test
+	void saveScoreTest(){
+
+	}
+
+	@Test
+	void destroyBlockTest() {
+
+	}
+
+	@Test
+	void resetBoardTest() {
+		
+	}
+
+	@Test
+	void ballMoveTest(){
+
+	}
+
+	@Test
+	void lifeLostTest(){
+
+	}
+
 
 }
