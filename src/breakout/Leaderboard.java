4a322e8a4c2635f6e1449e5f8af9999247a24065
Corinna Chang <corinnachang11@gmail.com>
2021-04-12 21:19:36 -0700

    Added new methods
    Getters, setters, and some ideas of how this class would work
    
    Using Collections.sort to sort highScores, but maybe treeset or something else is better

--- src/breakout/Leaderboard.java
+++ src/breakout/Leaderboard.java
@@ -1,5 +1,41 @@
 package breakout;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Leaderboard {
+	private ArrayList<Score> highScores; // should already be sorted from highest score to lowest
+	
+	public Leaderboard() {
+		//???
+	}
+	
+	public void addNewScore(Score newScore) {
+		highScores.add(newScore);
+		// sort array list?
+		Collections.sort(highScores);
+		// or use treeset & limit number of high scores
+			// like when adding new score, drop the lowest or cant add cuz score is too low
+	}
+	
+	// might need to change after implementing view
+	// just for junit testing for now
+	public String getAllScores() {
+		String allScores = "";
+		for (Score s : highScores)
+			allScores += s.toString();
+		return allScores;
+	}
+	
+	// do we really need this?
+	public ArrayList<Score> getTop5Score() {
+		ArrayList<Score> top5Score = new ArrayList<>();
+		for(int i = 0; i < 5; i++)
+			top5Score.add(highScores.get(i));
+		return top5Score;
+		
+	}
+	
+	
 
 }
