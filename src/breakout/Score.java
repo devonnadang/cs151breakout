7bca01005b79970885b4c8e8afeb6a4ff42406c1
Corinna Chang <corinnachang11@gmail.com>
2021-04-12 21:18:40 -0700

    Added new methods
    Getters, setters, and some ideas of how this class would work
    
    Implemented comparable interface for sorting in Leaderboard (likely to be changed later)

--- src/breakout/Score.java
+++ src/breakout/Score.java
@@ -1,7 +1,57 @@
 package breakout;
 
 // This Class keeps track of the user's score
+// game start: no username & score set to 0
+// game ends: set name if want to add to leaderboard
 
-public class Score {
+// *should score also store time?
+
+public class Score implements Comparable<Score> {
+	private String username;
+	private int currentScore;
+	
+	
+	public Score() {
+		username = "";
+		currentScore = 0;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public int getScore() {
+		return currentScore;
+	}
+	
+	// make boolean??? like if newUsername = "" or " " then return false username is not accepted/added
+	public void setUsername(String newUsername) {
+		
+		username = newUsername;
+	}
+	
+	public void incrementScore() {
+		currentScore += 5; // may change, depends on points per block
+	}
+	
+	// might need to change after implementing view
+	// for junit test
+	public String toString() {
+		return this.getUsername() + " " + this.getScore();
+	}
+
+	
+	@Override
+	// compare by score, then time?, then username?
+	// maybe don't need
+	public int compareTo(Score other) {
+		if (this.getScore() > other.getScore())
+			return 1;
+		else if (this.getScore() > other.getScore())
+			return -1;
+		// else compare time? compare username?
+			
+		return 0;
+	}
 	
 }
