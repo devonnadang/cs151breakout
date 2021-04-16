9e11bae3f5906e642d31935c78a3073d885dc833
Corinna Chang <corinnachang11@gmail.com>
2021-04-15 22:12:54 -0700

    Added Javadoc comments
    Added Javadoc comments for class and all methods.
    
    Changed getTop5Scores() method to getTop10Scores() to match junit test.

--- src/breakout/Leaderboard.java
+++ src/breakout/Leaderboard.java
@@ -1,22 +1,37 @@
 package breakout;
 
 import java.util.ArrayList;
+
+/**
+ * Keeps track of all scores and orders scores from highest to lowest.
+ */
+
 import java.util.Collections;
 
 public class Leaderboard {
-	private ArrayList<Score> highScores; // should already be sorted from highest score to lowest
+	private ArrayList<Score> highScores; // sorted from highest to lowest score
 	
+	
+	/**
+	 * Class constructor.
+	 */
 	public Leaderboard() {
-		//???
+		highScores = new ArrayList<>();
 	}
 	
+	
+	/**
+	 * Adds a new Score object to ArrayList highScores, then sorts all Score objects from highest to lowest.
+	 * 
+	 * @param Score object newScore to be added to ArrayList highScores.
+	 */
 	public void addNewScore(Score newScore) {
 		highScores.add(newScore);
-		// sort array list?
 		Collections.sort(highScores);
-		// or use treeset & limit number of high scores
+		// using collections sort for now but maybe treeset & limit number of high scores?
 			// like when adding new score, drop the lowest or cant add cuz score is too low
 	}
+	
 	
 	// might need to change after implementing view
 	// just for junit testing for now
@@ -27,15 +42,17 @@ public class Leaderboard {
 		return allScores;
 	}
 	
-	// do we really need this?
-	public ArrayList<Score> getTop5Score() {
-		ArrayList<Score> top5Score = new ArrayList<>();
-		for(int i = 0; i < 5; i++)
-			top5Score.add(highScores.get(i));
-		return top5Score;
-		
+	
+	/**
+	 * Gets the 10 highest scores from ArrayList highScores.
+	 * 
+	 * @return 10 highest Score objects saved.
+	 */
+	// for unit testing?
+	public ArrayList<Score> getTop10Score() {
+		ArrayList<Score> top10Score = new ArrayList<>();
+		for(int i = 0; i < 10; i++)
+			top10Score.add(highScores.get(i));
+		return top10Score;
 	}
-	
-	
-
 }
