ea5ff35c3f50e3abfe0219f2685494bdf01b4b1b
RashmiBoddukuri <rashmiboddukuri@gmail.com>
2021-05-01 20:11:59 -0700

    replaced view

--- src/breakout/view/View.java
+++ src/breakout/view/View.java
@@ -1,18 +1,23 @@
 package breakout.view;
 
-
+import breakout.controller.Message;
 import java.awt.Dimension;
+
+import java.util.concurrent.BlockingQueue;
 import javax.swing.JFrame;
 
 /**
  * Serves as the main view for Breakout. All other views will be within this one.
  */
 public class View extends JFrame{
+    private BoardView boardView;
+    private BlockingQueue<Message> queue;
 
-	/**
+    /**
      * Constructs the main view of Breakout.
      */
-    public View() {
+    public View(BlockingQueue<Message> queue) {
+        this.queue = queue;
         // Swing stuff
         setPreferredSize(new Dimension(500, 600));
         setResizable(true);
@@ -21,6 +26,25 @@ public class View extends JFrame{
         pack();
         setVisible(true);
     }
-    
 
+    /**
+     * This method creates the board and repaints the main view to make the board visible.
+     */
+    public void createBoardView() {
+        boardView = new BoardView(queue);
+        add(boardView);
+        revalidate();
+        repaint();
+    }
+
+    /**
+     * Updates BoardView with a new coordinate. This coordinate is going to be for the paddle's x coordinates, since
+     * that's the only coordinate that take in input and needs to be changed. This method is used with the message system.
+     * So, if you need to use the message system, then leave this method uncommented.
+     * @param newCoordinate
+     */
+    public void updateBoardView(int newCoordinate) {
+        boardView.setPaddleCoordinates(newCoordinate);
+        repaint();
+    }
 }
