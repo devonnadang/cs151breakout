036547ee78380e32ddc51a352507d1945c1e386d
Corinna Chang <corinnachang11@gmail.com>
2021-05-05 14:18:58 -0700

    new class for save score button

---
+++ src/breakout/controller/SaveScoreMessage.java
@@ -0,0 +1,19 @@
+package breakout.controller;
+
+public class SaveScoreMessage implements Message {
+	int score;
+	String username;
+	
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
