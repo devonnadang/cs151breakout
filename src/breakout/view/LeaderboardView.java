f1ade8839292fe80069220f036a5982124008c67
Corinna Chang <corinnachang11@gmail.com>
2021-05-07 01:51:13 -0700

    rename LeaderboardWindow to LeaderboardView

--- src/breakout/view/LeaderboardView.java
+++ src/breakout/view/LeaderboardView.java
@@ -2,23 +2,28 @@ package breakout.view;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import breakout.model.Leaderboard;
+import breakout.model.Score;
 
-public class LeaderboardWindow extends JFrame{
-	
+
+public class LeaderboardView extends JFrame{
+	private Leaderboard scoreList;
 	GridLayout layout = new GridLayout(0,3); // rows, columns, hgap, vgap
-    final static int maxGap = 20;
 
 
     /**
      * Constructs the main view of Breakout.
      */
-    public LeaderboardWindow() {
+    public LeaderboardView(Leaderboard scoreList) {
+    	
+    	this.scoreList = scoreList;
     	
     	JPanel panel = new JPanel();
     	panel.setLayout(layout);
@@ -30,13 +35,21 @@ public class LeaderboardWindow extends JFrame{
         panel.add(new JLabel("Score", SwingConstants.CENTER));
         
         // add Scores
+        int i = 1;
+        for (Score s : scoreList.getHighScores())
+        {
+        	panel.add(new JLabel(String.valueOf(i), SwingConstants.CENTER));
+        	panel.add(new JLabel(s.getUsername(), SwingConstants.CENTER));
+        	panel.add(new JLabel(String.valueOf(s.getScore()), SwingConstants.CENTER));
+        	i++;
+        }
 
         this.add(panel);
     	
     	this.setPreferredSize(new Dimension(500, 600));
     	this.setResizable(true);
     	this.setTitle("Leaderboard");
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     	this.pack();
     	this.setVisible(true);
         
